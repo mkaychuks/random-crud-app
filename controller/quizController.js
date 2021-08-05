8eeef934024925c398ee35efe2e9073be0e2b007
mkaychuks <sachufusi@gmail.com>
2021-08-05 11:16:44 -0700

    controller pips

---
+++ controller/quizController.js
@@ -0,0 +1,16 @@
+// get all quiz
+const getAllQuiz = (req, res) => {};
+
+// get a specific quiz
+const getOneQuiz = (req, res) => {};
+
+// create a quiz
+const createQuiz = (req, res) => {};
+
+// update quiz
+const updateQuiz = (req, res) => {};
+
+// delete a quiz
+const deleteQuiz = (req, res) => {};
+
+module.exports = { getAllQuiz, getOneQuiz, createQuiz, updateQuiz, deleteQuiz };
