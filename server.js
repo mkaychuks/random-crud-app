4d6e1c42de758886ff436dbe4680139e84fe10e6
mkaychuks <sachufusi@gmail.com>
2021-08-05 20:48:44 -0700

    init router changes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const quizRouter = require('./routes/quizRoutes');
 require('dotenv/config');
 const app = express();
+
+app.use(express.json());
+app.use('/', quizRouter);
 
 mongoose
   .connect(process.env.DB_URI, {
